dd3e90d31ada04913c2f5ebfb5cfc1b6191b9dca
Mauricio Sousa <lagrange.tesla@gmail.com>
2025-05-06 23:10:16 -0600

    Add logout controller

---
+++ app/Http/Controllers/Auth/LogoutController.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class LogoutController extends Controller
+{
+    public function logout(Request $request)
+    {
+        $request->user()->tokens()->delete();
+
+        return response()->json([
+            'message' => 'Logout successful.',
+        ]);
+    }
+}
