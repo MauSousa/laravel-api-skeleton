44e7b6d81cf4c7361e223db99db3ba61669fa41f
Mauricio Sousa <lagrange.tesla@gmail.com>
2025-05-06 21:04:12 -0600

    Add login controller

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+class LoginController extends Controller
+{
+    public function login(LoginRequest $request): JsonResponse
+    {
+        $request->validated();
+
+        $user = User::where('email', $request->email)->first();
+
+        if (! $user || ! Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect.'],
+            ]);
+        }
+
+        return response()->json([
+            'message' => 'Login successful.',
+            'access_token' => $user->createToken($request->email)->plainTextToken,
+        ]);
+    }
+}
